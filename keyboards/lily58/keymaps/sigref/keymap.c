129aa4d566ec3fa427e499b5538f4b8ac908aec4
sigref <22502259+sigref@users.noreply.github.com>
2022-08-20 17:39:02 +0900

    TAPPING TERMを125msに変更

--- keyboards/lily58/keymaps/sigref/keymap.c
+++ keyboards/lily58/keymaps/sigref/keymap.c
@@ -29,8 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_EQL,
   KC_LCTRL, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
-                        // KC_LALT, KC_LGUI, MO(_GRYPH), KC_SPC,  KC_ENT, MO(_MOVE), KC_BSPC, KC_RALT
-             KC_LALT, KC_LGUI, LT(_GRYPH, KC_LANG2),  KC_SPC,  KC_ENT,   LT(_MOVE, KC_LANG1), KC_BSPC, KC_RALT
+           KC_LALT, KC_LGUI, LT(_GRYPH, KC_LANG2),  KC_SPC,  KC_ENT,   LT(_MOVE, KC_LANG1), KC_BSPC, KC_RALT
 
 ),
 /* GRYPH
