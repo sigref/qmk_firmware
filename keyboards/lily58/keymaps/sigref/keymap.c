a6b7d6f36e27f0b1b20369b5fbf805de1255694c
sigref <22502259+sigref@users.noreply.github.com>
2024-01-27 21:53:05 +0900

    tap/holdの定義の仕方が間違ってたので修正

--- keyboards/lily58/keymaps/sigref/keymap.c
+++ keyboards/lily58/keymaps/sigref/keymap.c
@@ -13,7 +13,7 @@ enum Layer {
 };
 
 enum CustomKeyCode {
-    CK_GRV_ESC = SAFE_RANGE,
+    CK_ = SAFE_RANGE,
 };
 
 enum TapDanceKeyCode {
@@ -25,19 +25,19 @@ enum TapDanceKeyCode {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [L_DEFAULT] = LAYOUT(
-        CK_GRV_ESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                /**/              KC_6,    KC_7,    KC_8,    KC_9,   KC_0,            TD(TD_MIN_EQ),
-        KC_TAB,     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                /**/              KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,            KC_BSLS,
-        KC_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                /**/              KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN,         KC_QUOT,
-        KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   TD(TD_LBRC), /**/ TD(TD_RBRC), KC_N,    KC_M,    KC_COMM, KC_DOT, TD(TD_SLS_BSLS), KC_RSFT,
-                        KC_LALT, KC_LGUI, LT(L_GRYPH, KC_LNG2), KC_SPC,      /**/ KC_ENT,      LT(L_MOVE, KC_LNG1), KC_BSPC, KC_DEL
+        MT(KC_GRV, KC_ESC), KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                /**/              KC_6,    KC_7,    KC_8,    KC_9,   KC_0,            TD(TD_MIN_EQ),
+        KC_TAB,             KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                /**/              KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,            KC_BSLS,
+        KC_LCTL,            KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                /**/              KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN,         KC_QUOT,
+        KC_LSFT,            KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   TD(TD_LBRC), /**/ TD(TD_RBRC), KC_N,    KC_M,    KC_COMM, KC_DOT, TD(TD_SLS_BSLS), KC_RSFT,
+                                KC_LALT, KC_LGUI, LT(L_GRYPH, KC_LNG2), KC_SPC,      /**/ KC_ENT,      LT(L_MOVE, KC_LNG1), KC_BSPC, KC_DEL
     ),
 
     [L_GRYPH] = LAYOUT(
-        _______,    _______, _______, _______, _______, _______,          /**/          _______, _______, _______, _______, _______, _______,
-        KC_F1,      KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,            /**/          KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-        CK_GRV_ESC, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,          /**/          KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TILD,
-        _______,    _______, _______, _______, _______, _______, _______, /**/ _______, XXXXXXX, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
-                                      QK_BOOT, _______, _______, _______, /**/ _______, _______, KC_MENU, KC_RALT
+        _______, _______, _______, _______, _______, _______,          /**/          _______, _______, _______, _______, _______, _______,
+        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,            /**/          KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
+        KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,          /**/          KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TILD,
+        _______, _______, _______, _______, _______, _______, _______, /**/ _______, XXXXXXX, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
+                                   QK_BOOT, _______, _______, _______, /**/ _______, _______, KC_MENU, KC_RALT
     ),
 
     [L_MOVE] = LAYOUT(
@@ -66,14 +66,16 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 
 #pragma region process_record_user
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    if (keycode == CK_GRV_ESC) {
-        if (record->tap.count && record->event.pressed) {
-            tap_code16(KC_GRV);
+    switch (keycode) {
+        case MT(KC_GRV, KC_ESC):
+            if (record->event.pressed) {
+                if (record->tap.count == 1) {
+                    tap_code16(KC_GRV);
+                } else {
+                    tap_code16(KC_ESC);
+                }
+            }
             return false;
-        } else if (record->event.pressed) {
-            tap_code16(KC_ESC);
-            return false;
-        }
     }
 
     return true;
