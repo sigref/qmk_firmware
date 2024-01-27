591685122b9e8677e831fd4d6ba6340089797291
sigref <22502259+sigref@users.noreply.github.com>
2024-01-27 22:12:20 +0900

    マップ調整

--- keyboards/lily58/keymaps/sigref/keymap.c
+++ keyboards/lily58/keymaps/sigref/keymap.c
@@ -33,15 +33,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [L_GRYPH] = LAYOUT(
-        _______, _______, _______, _______, _______, _______,          /**/          _______, _______, _______, _______, _______, _______,
-        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,            /**/          KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-        KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,          /**/          KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TILD,
-        _______, _______, _______, _______, _______, _______, _______, /**/ _______, XXXXXXX, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
+        KC_ESC,  _______, _______, _______, _______, _______,          /**/          _______, _______, _______, _______,     _______,     _______,
+        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,            /**/          KC_F7,   KC_F8,   KC_F9,   KC_F10,      KC_F11,      KC_F12,
+        KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,          /**/          KC_CIRC, KC_AMPR, KC_ASTR, TD(TD_LBRC), TD(TD_RBRC), KC_TILD,
+        _______, _______, _______, _______, _______, _______, _______, /**/ _______, XXXXXXX, KC_UNDS, KC_PLUS, KC_LCBR,     KC_RCBR,     KC_PIPE,
                                    QK_BOOT, _______, _______, _______, /**/ _______, _______, KC_MENU, KC_RALT
     ),
 
     [L_MOVE] = LAYOUT(
-        _______, KC_1,    KC_2,    KC_3,     KC_4,    KC_5,             /**/          KC_6,    KC_7,    KC_8,    KC_9,     KC_0,    XXXXXXX,
+        KC_ESC,  KC_1,    KC_2,    KC_3,     KC_4,    KC_5,             /**/          KC_6,    KC_7,    KC_8,    KC_9,     KC_0,    XXXXXXX,
         _______, XXXXXXX, KC_UP,   XXXXXXX,  XXXXXXX, XXXXXXX,          /**/          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX,
         _______, KC_LEFT, KC_DOWN, KC_RIGHT, XXXXXXX, XXXXXXX,          /**/          KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, XXXXXXX, KC_RCTL,
         _______, KC_HOME, KC_END,  KC_PGUP,  KC_PGDN, XXXXXXX, XXXXXXX, /**/ XXXXXXX, XXXXXXX, KC_HOME, KC_END,  KC_PGUP,  KC_PGDN, _______,
