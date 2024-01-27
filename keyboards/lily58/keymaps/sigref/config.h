07201c88b701ca23aea917972a0f88957b59937c
sigref <22502259+sigref@users.noreply.github.com>
2024-01-28 00:50:23 +0900

    tapping termを調整

--- keyboards/lily58/keymaps/sigref/config.h
+++ keyboards/lily58/keymaps/sigref/config.h
@@ -27,7 +27,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define EE_HANDS
 
 #define QUICK_TAP_TERM 0
-#define TAPPING_TERM 125
+#define TAPPING_TERM 200
+#define TAPPING_TERM_PER_KEY
 
 #undef RGBLED_NUM
 #define RGBLIGHT_EFFECT_BREATHING
