d34309c760b60a1c41c47e497bb005fa2d93b25e
sigref <22502259+sigref@users.noreply.github.com>
2024-01-29 22:40:16 +0900

    quot|rctrlの定義を修正

--- keyboards/lily58/keymaps/sigref/keymap.c
+++ keyboards/lily58/keymaps/sigref/keymap.c
@@ -41,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [L_DEFAULT] = LAYOUT(
         MT(KC_GRV, KC_ESC), KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                /**/              KC_6,    KC_7,    KC_8,    KC_9,   KC_0,            TD(TD_MIN_EQ),
         KC_TAB,             KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                /**/              KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,            KC_BSLS,
-        KC_LCTL,            KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                /**/              KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN,         RCTL(KC_QUOT),
+        KC_LCTL,            KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                /**/              KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN,         MT(MOD_RCTL, KC_QUOT),
         KC_LSFT,            KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   TD(TD_LBRC), /**/ TD(TD_RBRC), KC_N,    KC_M,    KC_COMM, KC_DOT, TD(TD_SLS_BSLS), KC_RSFT,
                       CK_WWIN_MOPT, CK_WALT_MCMD, LT(L_GRYPH, KC_LNG2), KC_SPC,      /**/ KC_ENT,      LT(L_MOVE, KC_LNG1), KC_BSPC, KC_DEL
     ),
